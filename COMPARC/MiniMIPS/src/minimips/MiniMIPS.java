d65be425a27d3c52cc5d164f369bf69c3c6ef244
akosialex01 <alexander_punzalan@dlsu.edu.ph>
2016-03-18 00:06:46 +0800

    comparc

---
+++ COMPARC/MiniMIPS/src/minimips/MiniMIPS.java
@@ -0,0 +1,17 @@
+package minimips;
+
+/**
+ *
+ * @author Dreyasaur
+ */
+public class MiniMIPS 
+{
+    public static void main(String[] args) 
+    {
+        MainFrameGUI startGUI = new MainFrameGUI();
+        startGUI.setTitle("MiniMIPS");
+        startGUI.setResizable(false);
+        startGUI.setVisible(true);
+    }
+    
+}
