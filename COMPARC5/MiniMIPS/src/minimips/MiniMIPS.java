8e70f41ceefd491a5b474e338d9a93d4c6ffbf88
Andrea Olan <Dreyasaur@Andreas-MacBook-Pro.local>
2016-03-23 11:17:19 +0800

    COMPARC V5

---
+++ COMPARC5/MiniMIPS/src/minimips/MiniMIPS.java
@@ -0,0 +1,15 @@
+package minimips;
+
+public class MiniMIPS 
+{
+
+    public static void main(String[] args) 
+    {
+        MainFrameGUI startGUI = new MainFrameGUI();
+        startGUI.setTitle("MiniMIPS");
+        startGUI.setResizable(false);
+        startGUI.setVisible(true);
+        
+    }
+    
+}
